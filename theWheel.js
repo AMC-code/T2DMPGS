2ec649e02f223a398d49f30e90920897f2cbce58
AMC-code <amc06.developer@gmail.com>
2022-02-05 15:27:36 -0800

    testing new server

---
+++ theWheel.js
@@ -0,0 +1,6 @@
+// create three arrays
+// create a number of 123
+// have 10 array vaules with 3 digits in each value [123,456,789] etc.
+// loop through each array and based on the array value of the number so if the number is on 2 then in the array above if its in position 1 it would select 5
+// based on that digit selected that is what the array below its value will be set on and rinse and repeat for the other arrays
+// it should always produce the same outputs no matter how many times it is ran and you can use those numbers for some form of encryption or modify it so it uses the alphabet and replaces different letters in a file with selected alphabet letters chosen by the algorithm
